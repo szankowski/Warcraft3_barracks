07656a07f83ad86aa272a6d59a356452506d2f86
Chrystian Szankowski <chrys@audioeacustica.com>
2015-07-01 03:01:58 +0000

    almost there

--- lib/footman.rb
+++ lib/footman.rb
@@ -6,5 +6,17 @@ class Footman < Unit
   def initialize
     super(60, 10)
   end
+  
+  def attack!(enemy)
+
+    if enemy.is_a?(Barracks)
+     enemy.damage(@attack_power / 2)
+    else
+      raise DeadError, "Attacker is dead!" if dead?
+      raise DeadError, "Victim is dead!" if enemy.dead?
+     enemy.damage(@attack_power)
+    end
+
+  end
 
 end
